4a34a34eaf883cfa2bf2a0e8c86b1a5fad003451
wangdinghai <672381992@qq.com>
2020-04-19 21:49:42 +0800

    init

--- eurekademo/eurekaserver7001/src/main/java/com/wdh/springcloud/EurekaServer7001.java
+++ eurekademo/eurekaserver7001/src/main/java/com/wdh/springcloud/EurekaServer7001.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServer7001 {
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer7001.class, args);
     }
+
 }
