8b28689972630c0376e82092c2ec79d6e8917dec
wangdinghai <672381992@qq.com>
2020-04-19 21:29:30 +0800

    init

--- eurekademo/paydemo9002/src/main/java/springcloud/PayDemoApplication9002.java
+++ eurekademo/paydemo9002/src/main/java/springcloud/PayDemoApplication9002.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class PayDemoApplication9002 {
+
     public static void main(String[] args) {
         SpringApplication.run(PayDemoApplication9002.class, args);
     }
+
 }
